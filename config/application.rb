4307679c963f92e2a6efbdceb0a3aae59923eb17
smcommits <sm.anas@outlook.com>
2021-08-20 17:05:55 +0530

    Add origin for root app

--- config/application.rb
+++ config/application.rb
@@ -41,7 +41,7 @@ end
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'http://localhost:3000'
+    origins 'https://stack-show.herokuapp.com'
     resource '*', headers: :any, methods: %i[get post delete patch put], credentials: true
   end
 end
