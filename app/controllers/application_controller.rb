21aabe23db73f8afead9ee72a482632402fd5f48
Anas <anas@schmiede.one>
2023-03-09 05:17:20 +0530

    remove delay from applicatio controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,7 @@
 class ApplicationController < ActionController::API
   include ActionController::Cookies
   include DeviseTokenAuth::Concerns::SetUserByToken
-
   before_action :configure_permitted_parameters, if: :devise_controller?
-  before_action :add_delay
-
   protected
 
   def configure_permitted_parameters
@@ -15,9 +12,5 @@ class ApplicationController < ActionController::API
     {
       next_page: collection.next_page
     }
-  end
-
-  def add_delay
-    sleep(5)
   end
 end
